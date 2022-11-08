391d1717827da7b94687ca58eb7cb39f3ad97fc4
plumy828 <plumy828@gmail.com>
2022-11-08 12:54:32 +0900

    Create 17_example_1.ino

---
+++ 17_example_1.ino
@@ -0,0 +1,74 @@
+#include <Servo.h>
+
+// Arduino pin assignment
+
+#define PIN_POTENTIOMETER A3 // Potentiometer at Pin A3
+#define PIN_IRSENSOR A0
+#define PIN_LED 9
+// Add IR Sensor Definition Here !!!
+#define PIN_SERVO 10
+
+#define _DUTY_MIN 553  // servo full clock-wise position (0 degree)
+#define _DUTY_NEU 1476 // servo neutral position (90 degree)
+#define _DUTY_MAX 2399 // servo full counter-clockwise position (180 degree)
+#define DIST_MIN 100.0
+#define DIST_MAX 250.0
+#define _EMA_ALPHA 0.6
+
+#define LOOP_INTERVAL 50   // Loop Interval (unit: msec)
+
+Servo myservo;
+unsigned long last_loop_time;   // unit: msec
+float dist_ema;
+float dist_prev = DIST_MIN;
+
+void setup()
+{
+  myservo.attach(PIN_SERVO); 
+  myservo.writeMicroseconds(_DUTY_NEU);
+  pinMode(PIN_LED, OUTPUT);
+  pinMode(PIN_IRSENSOR, INPUT);
+  digitalWrite(PIN_LED, 1);
+  Serial.begin(115200);
+}
+
+void loop()
+{
+  unsigned long time_curr = millis();
+  int a_value, duty;
+  float dist;
+
+  // wait until next event time
+  if (time_curr < (last_loop_time + LOOP_INTERVAL))
+    return;
+  last_loop_time += LOOP_INTERVAL;
+
+  // Remove Next line !!!
+  // Read IR Sensor value !!!
+  a_value = analogRead(PIN_IRSENSOR);
+  // Convert IR sensor value into distance !!!
+  dist = (6762.0/(a_value - 9) - 4.0) * 10.0 - 60.0;
+  // we need distance range filter here !!!
+  if (dist < DIST_MIN || dist > DIST_MAX) {
+    dist = dist_prev;
+    digitalWrite(PIN_LED, 1);
+  } else {
+    dist_prev = dist;
+    digitalWrite(PIN_LED, 0);
+  }
+  // we need EMA filter here !!!
+  dist_ema = _EMA_ALPHA * dist + (1.0 - _EMA_ALPHA) * dist_ema;
+
+  // map distance into duty
+  duty = map(a_value, 0, 1023, _DUTY_MIN, _DUTY_MAX);
+  myservo.writeMicroseconds(duty);
+
+  // print IR sensor value, distnace, duty !!!
+  Serial.print("MIN: ");      Serial.print(DIST_MIN);
+  Serial.print(" ,IR: ");     Serial.print(a_value);
+  Serial.print(" ,dist");     Serial.print(dist);
+  Serial.print(" ,ema: ");    Serial.print(dist_ema);
+  Serial.print(" ,servo: ");  Serial.print(duty);
+  Serial.print(" ,MAX: ");    Serial.print(DIST_MAX);
+  Serial.println("");
+}
